43bd4f2d8121cac6f943ea56eafd3fe559ad2c0c
Sanusi Abdulazeez <sanusiabdulazeezadebayo1@gmail.com>
2025-11-09 14:37:10 +0100

    Added config sample

---
+++ config_sample.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'BYBIT' => [
+        'API_KEY' => 'YOURAPIKEYHERE',
+        'SECRET_KEY' => 'YOURSECRETKEYHERE',
+        'ENDPOINT' => 'https://api.bybit.com',
+        'deposit_fee' => 0,
+    ]
+];
